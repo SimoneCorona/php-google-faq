aa292f2e20121872792d829cdbb0e2c5f3e4e6ab
SimoneCorona <corona.simone93@gmail.com>
2022-06-09 17:43:18 +0200

    add other css

--- index.php
+++ index.php
@@ -69,7 +69,7 @@ $frequent_question= [
                 <li>Norme sulla privacy</li>
                 <li>Termini di servizio</li>
                 <li>Tecnologie</li>
-                <li>Domande frequenti</li>
+                <li class="active">Domande frequenti</li>
             </ul>
         </div>
     </header>
