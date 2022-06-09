a3c428d839855df7a8421c396fb03f31a2c1ec51
SimoneCorona <corona.simone93@gmail.com>
2022-06-09 17:17:03 +0200

    css seems infinite

--- index.php
+++ index.php
@@ -84,7 +84,23 @@ $frequent_question= [
     </main>
 
     <footer>
-
+        <div>
+            <ul>
+                <li>Google</li>
+                <li>Tutto su Google</li>
+                <li>Privacy</li>
+                <li>Termini</li>
+            </ul>
+        </div>
+        <div>
+            <select>
+                <option value="">Italiano</option>
+                <option value="">English</option>
+                <option value="">Spanish</option>
+                <option value="">Russian</option>
+                <option value="">French</option>
+            </select>
+        </div>
     </footer>
 </body>
 </html>
